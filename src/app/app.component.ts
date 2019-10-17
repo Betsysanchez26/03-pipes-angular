ef20a338be8b9dc3b497f9192d41ce3ed10802cd
Betsy Sanchez <bedesanchezca@ittepic.edu.mx>
2019-10-17 13:03:12 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = 'Betsy';
+
+  nombre2 = 'beTsy del carmen sánCHez carrIllo';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+  nombre: 'Logan',
+  clave: 'Wolverine',
+  edad: 500,
+  direccion: {
+    calle: 'Primera',
+    casa: '19'
+  }
+  };
+
+  valorDePromesa = new Promise ((resolve, reject) => {
+    setTimeout(() => resolve ('llego la data'), 3000);
+  });
+
+  fecha = new Date();
+
+  video = 'B5B5pPVEwyw';
+
+  activar: Boolean = true;
 }
